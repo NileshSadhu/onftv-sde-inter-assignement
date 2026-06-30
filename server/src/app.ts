549adb346e83b04b8b229913ef59cde0afe46812
nileshsadhu562@gmail.com <nileshsadhu562@gmail.com>
2026-06-30 15:59:55 +0530

    server/src/app.ts

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,15 +1,21 @@
 import express, { type Request, type Response } from "express";
 import cors from "cors";
+import errorHandler from "./middleware/error.middleware.js";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(errorHandler);
 
 app.get("/", (req: Request, res: Response) => {
   return res.status(200).json({
     message: "Health Check API is working fine.",
   });
 });
+
+import authRoute from "./routes/auth.route.js";
+
+app.use("/api/auth", authRoute);
 
 export default app;
