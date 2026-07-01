e34d9779a7fd0a0ddf92c3bc1e46edaa0e5853f9
nileshsadhu562@gmail.com <nileshsadhu562@gmail.com>
2026-07-01 17:06:23 +0530

    Increase jwt token time

--- server/src/utils/generateToken.ts
+++ server/src/utils/generateToken.ts
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken";
 
 const generateToken = (userId: string) => {
   return jwt.sign({ id: userId }, process.env.JWT_SECRET!, {
-    expiresIn: "1h",
+    expiresIn: "1d",
   });
 };
 
